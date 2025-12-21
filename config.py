0a1dd44e473a74e813da00badd45edf7051ed3e0
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-12-21 20:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 from os import environ
 
 class Config(object):
-    TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
     APP_ID = int(environ.get("APP_ID", "21288218"))
     API_HASH = environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_USER_SESSION = environ.get('TG_USER_SESSION')
-    FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
+    TG_USER_SESSION = environ.get("TG_USER_SESSION", "")
     ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
-    STATUS_CHAT = -1003118824708
-    TO_CHAT = -1001506021749 
-    SKIP_CHAT = -1001631481154
-    SKIP_MSG_ID = 14
-    
+    STATUS_CHAT_GROUP_ID = -1003118824708
+    STATUS_CHANNEL_ID = -1001631481154
+    STATUS_CHANNEL_MSG_ID = 19
